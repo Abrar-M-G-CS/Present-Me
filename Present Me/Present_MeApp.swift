a395485d31155958b0cab40608b15b74da16c0ab
Abrar-M-G-CS <abrarghandurah@gmail.com>
2023-10-08 11:07:41 +0300

    Initial Commit

---
+++ Present Me/Present_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Present_MeApp.swift
+//  Present Me
+//
+//  Created by Abrar Ghandurah on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Present_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

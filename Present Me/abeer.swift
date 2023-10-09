859608086cbe7e1b9d79e344f4222794b5f38079
AbeerAlyaeesh <abeer.alyaeesh3@gmail.com>
2023-10-09 09:34:03 +0300

    abeer

---
+++ Present Me/abeer.swift
@@ -0,0 +1,18 @@
+//
+//  abeer.swift
+//  Present Me
+//
+//  Created by Abeer Alyaeesh on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct abeer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    abeer()
+}
